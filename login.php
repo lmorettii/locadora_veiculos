3d5ab9966b15b7d3dffe4bfc7ed8af9c99a19ba3
lmorettii <lucasmorete09@gmail.com>
2025-04-14 11:29:37 -0300

    Locadora de veiculos

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-
+// backend
 ?>
 
 <!DOCTYPE html>
@@ -7,42 +7,69 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - Locadora</title>
+    <title>Login - Locadora de veículos</title>
+    <!-- Link do bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Link dos ícones -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- CSS Interno -->
     <style>
-
+        .login-container{
+            max-width:400px;
+            margin: 100px auto;
+        }
+        .password-toggle{
+            position: absolute;
+            right: 15px;
+            top: 50%;
+            transform: translateY(-50%);
+            cursor: pointer;
+        }
     </style>
 </head>
 <body class="bg-light">
     <div class="login-container">
         <div class="card">
+            <!-- Título do card -->
             <div class="card-header">
                 <h4 class="mb-1">Login</h4>
             </div>
+
+            <!-- Corpo do card -->
             <div class="card-body">
 
-                <form action="post" class="needs-valedation" novalidate>
+                <form action="post" class="needs-validation" novalidate>
                     <input type="hidden">
 
                     <div class="mb-3">
                         <label for="user" class="form-label">
-                        Usuário:
+                            Usuário:
                         </label>
-                        <input type="text" name="username" class="form-control" require autocomplete="off" placeholder="Digite o usuário">
+                        <input type="text" name="username" class="form-control" required autocomplete="off" placeholder="Digite o usuário">
                     </div>
-                    
+
                     <div class="mb-3 position-relative">
                         <label for="password" class="form-label">
                             Senha:
                         </label>
-                        <input type="text">
+                        <input type="password" name="password" id="password" class="form-control" required>
+                        <span class="password-toggle mt-3" onclick="togglePassword()"><i class="bi bi-eye"></i></span>
                     </div>
-                </form>
 
+                    <button type="submit" class="btn btn-warning w-100">Entrar</button>
+                </form>
             </div>
         </div>
     </div>
-    
+    <!-- Tabela -->
+        <div class="row mt-4">
+            <div class=""></div>
+        </div>
+    <script>
+        function togglePassword() {
+            let passwordInput = document.getElementById('password');
+            passwordInput.type = (passwordInput.type === 'password') ? 'text' : 'password';
+        }
+    </script>
 </body>
 </html>
